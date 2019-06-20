1ce1f5c4aa4368f4cf764f638c98c82eaf71ab81
hysspoluo <517645428@qq.com>
2019-06-20 16:28:53 +0800

    功能完善

--- show_window.py
+++ show_window.py
@@ -4,6 +4,8 @@ from PyQt5 import QtGui
 import sys
 import  _thread
 import time
+import  conversion
+
 
 #主窗体初始化类
 
@@ -22,11 +24,55 @@ class Main(QMainWindow,Ui_MainWindow):#两个窗口类
         self.iput_image.setPixmap(input_image)#设置初始化图片
         output_image = QtGui.QPixmap('output_test.png')
         self.output_image.setPixmap(output_image)
+    #打开图片文件
+    def openFile(self):
+        openfile_name = QFileDialog.getOpenFileName()
+        if openfile_name[0] !="":
+            self.input_path = openfile_name[0]
+            self.show_input_img(self.input_path)
+    #显示输入的图片
+    def show_input_img(self,file_path):
+        input_img = QtGui.QPixmap(file_path)
+        self.iput_image.setPixmap(input_img)#设置图片
+
+    #启动图片转换
+    def start_conversion(self):
+        if hasattr(main,"input_path"):
+            #self.gif = QtGui.Qmovie("loding.gif")
+            #self.gif = QtGui.QMovie('loding.gif')
+            #self.loading.setMovie(self.gif)
+            #self.gif.start()
+            #_thread.start_new_thread(ambda: self.is_conversion(main.input_path),())
+            _thread.start_new_thread(lambda: self.is_conversion(main.input_path), ())
+        else:
+            print("没有图片")
+
+    def is_conversion(self,file_path):
+        t = str(int(time.time()))
+        #转换后的字符画图路径
+        export_path = "export_img"+t+".png"
+        input_char = main.textEdit.toPlainText()
+        definition = main.comboBox.currentText()
+
+        is_over = conversion.picture_conversion(file_path,export_path,input_char,definition)
+        if is_over == False:
+            #self.loding.clear()
+            #self.loding.clear()  # 转换完毕就将等待gif图片清理掉
+            main.show_export_img(export_path)
+
+
+    def show_export_img(self,file_path):
+        export_img = QtGui.QPixmap(file_path)
+        self.output_image.setPixmap(export_img)
+
+
 
 
 if __name__ == "__main__":
     #入口函数
     app = QApplication(sys.argv)
-    main1 = Main()
-    main1.show()
+    main = Main()
+    main.pushButton_inPut.clicked.connect(main.openFile)
+    main.pushButton_conversion.clicked.connect(main.start_conversion)
+    main.show()
     sys.exit(app.exec())
